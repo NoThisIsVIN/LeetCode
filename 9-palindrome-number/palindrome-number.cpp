2ac720619478a248769569064969a798b14007dd
VIN <99738631+NoThisIsVIN@users.noreply.github.com>
2026-03-30 22:00:32 +0530

    Time: 4 ms (31.54%) | Memory: 8.7 MB (37.21%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+
+        int m= 0; 
+        long long r = 0;
+        int o = x;
+        if ( x < 0 ){
+            return false;
+        }
+
+        while( x !=0 ){
+            m = x%10;
+            r = r*10 + m;
+            x = x/10;
+            }
+
+        if ( o == r ){
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+};
