f1e519c27e30355298feca294393cc6619cf4da6
VIN <99738631+NoThisIsVIN@users.noreply.github.com>
2026-04-02 21:03:14 +0530

    Time: 1 ms (98.12%) | Memory: 42.9 MB (17.25%) - LeetSync

---
+++ 258-add-digits/add-digits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int addDigits(int num) {
+     if( num ==0 ){
+            return num;
+        } 
+        while (num >= 10){
+        int s = 0;
+        
+            while(num>0){
+            s += num % 10;
+           num = num/10;
+            }
+        num = s;
+                    }
+
+    return num;}
+}
