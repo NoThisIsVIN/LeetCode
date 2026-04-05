9d58acab9375ab2cea57c2fc2cbd4ed7b4c30ad8
VIN <99738631+NoThisIsVIN@users.noreply.github.com>
2026-04-05 16:26:50 +0530

    Time: 10 ms (99.70%) | Memory: 97.7 MB (11.36%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -4,9 +4,8 @@ class Solution {
   HashSet<Integer> seen = new HashSet<>(); 
 
         for (int num : nums) {
-            if (!seen.add(num)) {
+            if (!seen.add(num))
                 return true; 
-            }
         }
         
         return false;
