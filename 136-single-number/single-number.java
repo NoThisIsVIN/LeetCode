1fa2bc43640286e99234828688c3e4016c207f5b
VIN <99738631+NoThisIsVIN@users.noreply.github.com>
2026-04-06 20:44:10 +0530

    Time: 1 ms (99.98%) | Memory: 46.9 MB (54.91%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for(int i =0; i<nums.length; i++){
+            result = result ^ nums[i]; }
+   return result; }}
