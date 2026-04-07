7fc286f6de03a159aa65bea77baa8a8758e2085f
VIN <99738631+NoThisIsVIN@users.noreply.github.com>
2026-04-07 20:29:05 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (92.05%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.java
+++ 2058-concatenation-of-array/concatenation-of-array.java
@@ -1,8 +1,7 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
-        int n = nums.length;
-        int[] r = new int[2*n];
-        System.arraycopy(nums, 0, r, 0, n);
-        System.arraycopy(nums, 0, r, n, n);
+        int[] r = new int[2*nums.length];
+        System.arraycopy(nums, 0, r, 0, nums.length);
+        System.arraycopy(nums, 0, r, nums.length, nums.length);
     return r;}
 }
