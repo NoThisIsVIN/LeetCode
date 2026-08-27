88adecf283acf29de876fc1d2091755ea8895eb5
VIN <99738631+NoThisIsVIN@users.noreply.github.com>
2026-08-27 20:51:10 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (43.29%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,11 +1,11 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-            int count = 0;
+            int c= 0;
         for(int i=0; i<nums.length; i++){ 
             if(nums[i] != val){
-                nums[count] = nums[i];
-                count++;
+                nums[c] = nums[i];
+                c++;
             }   
         }
-    return count;}
+    return c;}
 }
