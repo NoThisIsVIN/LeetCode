85bc571a66ee56ed04abad8883433f4233be0726
VIN <99738631+NoThisIsVIN@users.noreply.github.com>
2026-08-30 11:10:05 +0530

    Time: 5 ms (12.79%) | Memory: 273.3 MB (7.28%) - LeetSync

---
+++ 189-rotate-array/rotate-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+
+        int[] r = new int[nums.length];
+        int c =0;
+        k = k % nums.length;
+        for ( int i =nums.length -k ; i< nums.length  ; i++){
+            r[c] = nums[i];
+            c++;
+            
+        }
+
+        for ( int i = 0; i< nums.length -k ; i++){
+            r[c] = nums[i];
+            c++;
+        }
+
+        for (int i = 0; i<nums.length; i++){
+            nums[i] = r[i];
+        }
+        
+
+        
+    }
+}
