a9f592654733e955997dfdd5f8a8b30179ba9068
VIN <99738631+NoThisIsVIN@users.noreply.github.com>
2026-08-31 21:03:25 +0530

    Time: 7 ms (47.21%) | Memory: 47.5 MB (11.18%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,14 +1,49 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-    int i, j;
-    for(i=0; i<nums.length; i++){
 
-        for( j = i+1 ; j< nums.length; j++){
 
-         if( nums[i] + nums [j] == target){
-            return new int[] { i, j};
-          }
+        int[] a = new int[nums.length];
+        a = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(nums);
+
+        int low = 0;
+
+        int high = nums.length - 1;
+
+        int[] r = new int[2];
+
+        while(low < high){
+
+            if (nums[low]+nums[high] == target){
+                r[0] = nums[low];
+                r[1] = nums[high];
+                break;
+            }
+
+            if (nums[low]+nums[high] < target){
+                low++;
+            }
+            if (nums[low]+nums[high] > target){
+                high--;
+            }
+
         }
+   int index = -1;
+
+for (int i = 0; i < a.length; i++) {
+    if (a[i] == r[0]) {
+        r[0] = i;
+        index = i;
+        break;
     }
-    return new int[] {};} 
+}
+
+for (int i = 0; i < a.length; i++) {
+    if (i != index && a[i] == r[1]) {
+        r[1] = i;
+        break;
+    }
+}
+        
+    return r;}
 }
