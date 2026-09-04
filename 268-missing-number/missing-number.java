0320dce52cd1b43127cc1de96be86cd6c3b7bd00
VIN <99738631+NoThisIsVIN@users.noreply.github.com>
2026-09-04 16:54:35 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (91.69%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,30 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        
-        Arrays.sort(nums);
 
-    int p1 =0;
-    int p2 =1;
+        int n = nums.length;
+        int sum = n*(n+1)/2;
 
-    if (nums.length != nums[nums.length-1]){
-
-        return nums[nums.length-1] + 1;
-
-    }
-
-    if (nums[0] != 0){
-        return 0;
-    }
-
-
-    while(p2< nums.length){
-
-        if (nums[p2]- nums[p1] == 1){
-            p1++;
-            p2++;
-        }else{
-            return nums[p1] + 1;
+        for ( int i =0; i< nums.length; i++){
+            sum -= nums[i];
         }
-    }
-    return -1;}
+    return sum;}
 }
