e77bf5fa525660961e74579279ced77c4a266be1
VIN <99738631+NoThisIsVIN@users.noreply.github.com>
2026-09-11 22:24:59 +0530

    Time: 5 ms (98.19%) | Memory: 47.4 MB (5.92%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,12 +1,23 @@
 class Solution {
     public int firstUniqChar(String s) {
 
-        for(int i =0 ; i<s.length(); i++){
-            char n = s.charAt(i);
-            
-          if(s.indexOf(n) == s.lastIndexOf(n))
-                return i; 
+        int[] count = new int[26];
+
+       for(int i=0; i<s.length(); i++){
+
+        count[s.charAt(i) - 'a']++;
+
+       }
+
+       for(int i=0; i<s.length(); i++){
+
+        if(count[s.charAt(i) - 'a'] == 1){
+            return i;
         }
+
+       }
+
+    
         
     return -1;}
 }
