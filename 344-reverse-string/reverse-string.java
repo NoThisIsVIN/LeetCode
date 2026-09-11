e846f4123a4029c2b97d828a415380755cc5a1b5
VIN <99738631+NoThisIsVIN@users.noreply.github.com>
2026-09-11 20:04:40 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (73.61%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void reverseString(char[] s) {
+
+        int p1 = 0; 
+        int p2 = s.length - 1;
+
+        while(p2>p1){
+            
+            char temp;
+
+            temp = s[p2];
+
+            s[p2] = s[p1];
+
+            s[p1] = temp;
+
+            p1++;
+            p2--;
+        }
+        
+    }
+}
